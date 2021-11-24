b3fb98fbfb9fced7038d065da7bb169ccfb85e9a
VAMSIKRISHNAAMPAGOWNI <94233420+VAMSIKRISHNAAMPAGOWNI@users.noreply.github.com>
2021-11-24 06:42:45 +0530

    Create demo.h

---
+++ Unit_Testing/demo.h
@@ -0,0 +1,24 @@
+/**
+ * @file demo.h
+ * @author vamsi
+ * @brief Demo program to showcase the Unit testing examples
+ * @version 0.1
+ * @date 24-11-2021
+ * 
+ */
+#ifndef __DEMO_H__
+#define __DEMO_H__
+
+#include "stdio.h"
+#include "assert.h"
+
+/**
+ * @brief Function to perform division of 2 interger values
+ * 
+ * @param num1 Numerator
+ * @param num2 Denominator
+ * @return flaot Result of the division
+ */
+float division(int num1, int num2);
+
+#endif // #ifndef __DEMO_H__
